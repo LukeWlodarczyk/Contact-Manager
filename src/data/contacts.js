4296d253ecf16b57e66b08dd529c31ac7272d100
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-08-27 01:24:10 +0200

    update default data

--- src/data/contacts.js
+++ src/data/contacts.js
@@ -2,19 +2,19 @@ export default [
 	{
 		name: 'Luke Shaw',
 		email: 'luke.shaw@gmail.com',
-		phone: 889786784,
-		id: 1,
+		phone: '889786784',
+		id: '1',
 	},
 	{
 		name: 'Emma Shaw',
 		email: 'emma.shaw@gmail.com',
-		phone: 856786784,
-		id: 2,
+		phone: '856786784',
+		id: '2',
 	},
 	{
 		name: 'Bill Trenton',
 		email: 'bill.trenton@gmail.com',
-		phone: 889781884,
-		id: 3,
+		phone: '889781884',
+		id: '3',
 	},
 ];
