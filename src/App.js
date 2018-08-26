5cf9c154079fe1407f2486d1ed09b1a966834058
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-08-26 23:59:48 +0200

    routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Provider } from './state-management';
+
+import Header from './components/layout/Header';
+import Contacts from './components/contacts/Contacts';
+import NotFound from './components/layout/NotFound';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div>Hello world</div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<Provider>
+				<Router>
+					<div className="App">
+						<Header />
+						<div className="container">
+							<Switch>
+								<Route exact path="/" component={Contacts} />
+								<Route exact path="/" component={NotFound} />
+							</Switch>
+						</div>
+					</div>
+				</Router>
+			</Provider>
+		);
+	}
 }
 
 export default App;
