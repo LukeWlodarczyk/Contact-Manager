04f395f2eb0a45d2c6b452004d493aa38ce3246a
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-08-27 00:23:41 +0200

    default data

--- src/state-management/Provider.js
+++ src/state-management/Provider.js
@@ -1,8 +1,32 @@
 import React, { Component } from 'react';
 import Context from './context';
 
+import contacts from '../data/contacts';
+
 const reducer = (state, action) => {
 	switch (action.type) {
+		case 'DELETE_CONTACT':
+			return {
+				...state,
+				contacts: state.contacts.filter(
+					contact => contact.id !== action.payload
+				),
+			};
+		case 'ADD_CONTACT':
+			return {
+				...state,
+				contacts: [action.payload, ...state.contacts],
+			};
+		case 'UPDATE_CONTACT':
+			return {
+				...state,
+				contacts: state.contacts.map(
+					contact =>
+						contact.id === action.payload.id
+							? (contact = action.payload)
+							: contact
+				),
+			};
 		default:
 			return state;
 	}
@@ -10,7 +34,7 @@ const reducer = (state, action) => {
 
 class Provider extends Component {
 	state = {
-		contacts: [],
+		contacts: [...contacts],
 	};
 
 	dispatch = action => this.setState(state => reducer(state, action));
