62f331895e774b984b0224af75ee7099e019dc86
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-08-27 01:24:46 +0200

    add state

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
-export default () => {
-	return (
-		<div>
-			<h1 className="display-4 text-center">
-				<span className="danger">404</span> Page Not Found
-			</h1>
-			<p className="lead text-center">Sorry, this page does not exist...</p>
-		</div>
-	);
-};
+export default ({ location }) => (
+	<div>
+		<h1 className="display-4 text-center">
+			<span className="danger">404</span> Page Not Found
+		</h1>
+		<p className="lead text-center">
+			Sorry,{' '}
+			{location.state
+				? `contact with id ---> ${location.state.id}`
+				: 'this page'}{' '}
+			does not exist...
+		</p>
+	</div>
+);
