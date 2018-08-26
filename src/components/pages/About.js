23a30f792fec23310f409727fa20af581d19171f
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-08-27 00:43:30 +0200

    about

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default () => {
+	return (
+		<div>
+			<h1 className="display-4">About Contact Manager</h1>
+			<p className="lead">
+				Simple app created with ReactJS and new React Context API for
+				state-management.
+			</p>
+		</div>
+	);
+};
